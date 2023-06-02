4bfbb975fedcef2a8e93792df39d4598a1f3432a
Brian Downs <brian.downs@gmail.com>
2023-06-02 16:00:37 -0700

    add binutils
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- cmd/plugin/commands/ingresses/ingresses_test.go
+++ cmd/plugin/commands/ingresses/ingresses_test.go
@@ -14,59 +14,59 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package ingresses
+// package ingresses
 
-import (
-	"testing"
-)
+// import (
+// 	"testing"
+// )
 
-func TestGetIngressInformation(t *testing.T) {
+// func TestGetIngressInformation(t *testing.T) {
 
-	// testcases := map[string]struct {
-	// 	ServiceBackend *networking.IngressServiceBackend
-	// 	wantName       string
-	// 	wantPort       intstr.IntOrString
-	// }{
-	// 	"empty ingressServiceBackend": {
-	// 		ServiceBackend: &networking.IngressServiceBackend{},
-	// 		wantName:       "",
-	// 		wantPort:       intstr.IntOrString{},
-	// 	},
-	// 	"ingressServiceBackend with port 8080": {
-	// 		ServiceBackend: &networking.IngressServiceBackend{
-	// 			Name: "test",
-	// 			Port: networking.ServiceBackendPort{
-	// 				Number: 8080,
-	// 			},
-	// 		},
-	// 		wantName: "test",
-	// 		wantPort: intstr.IntOrString{
-	// 			Type:   intstr.Int,
-	// 			IntVal: 8080,
-	// 		},
-	// 	},
-	// 	"ingressServiceBackend with port name a-svc": {
-	// 		ServiceBackend: &networking.IngressServiceBackend{
-	// 			Name: "test",
-	// 			Port: networking.ServiceBackendPort{
-	// 				Name: "a-svc",
-	// 			},
-	// 		},
-	// 		wantName: "test",
-	// 		wantPort: intstr.IntOrString{
-	// 			Type:   intstr.String,
-	// 			StrVal: "a-svc",
-	// 		},
-	// 	},
-	// }
+// testcases := map[string]struct {
+// 	ServiceBackend *networking.IngressServiceBackend
+// 	wantName       string
+// 	wantPort       intstr.IntOrString
+// }{
+// 	"empty ingressServiceBackend": {
+// 		ServiceBackend: &networking.IngressServiceBackend{},
+// 		wantName:       "",
+// 		wantPort:       intstr.IntOrString{},
+// 	},
+// 	"ingressServiceBackend with port 8080": {
+// 		ServiceBackend: &networking.IngressServiceBackend{
+// 			Name: "test",
+// 			Port: networking.ServiceBackendPort{
+// 				Number: 8080,
+// 			},
+// 		},
+// 		wantName: "test",
+// 		wantPort: intstr.IntOrString{
+// 			Type:   intstr.Int,
+// 			IntVal: 8080,
+// 		},
+// 	},
+// 	"ingressServiceBackend with port name a-svc": {
+// 		ServiceBackend: &networking.IngressServiceBackend{
+// 			Name: "test",
+// 			Port: networking.ServiceBackendPort{
+// 				Name: "a-svc",
+// 			},
+// 		},
+// 		wantName: "test",
+// 		wantPort: intstr.IntOrString{
+// 			Type:   intstr.String,
+// 			StrVal: "a-svc",
+// 		},
+// 	},
+// }
 
-	// for title, testCase := range testcases {
-	// 	gotName, gotPort := serviceToNameAndPort(testCase.ServiceBackend)
-	// 	if gotName != testCase.wantName {
-	// 		t.Fatalf("%s: expected '%v' but returned %v", title, testCase.wantName, gotName)
-	// 	}
-	// 	if gotPort != testCase.wantPort {
-	// 		t.Fatalf("%s: expected '%v' but returned %v", title, testCase.wantPort, gotPort)
-	// 	}
-	// }
-}
+// for title, testCase := range testcases {
+// 	gotName, gotPort := serviceToNameAndPort(testCase.ServiceBackend)
+// 	if gotName != testCase.wantName {
+// 		t.Fatalf("%s: expected '%v' but returned %v", title, testCase.wantName, gotName)
+// 	}
+// 	if gotPort != testCase.wantPort {
+// 		t.Fatalf("%s: expected '%v' but returned %v", title, testCase.wantPort, gotPort)
+// 	}
+// }
+// }
